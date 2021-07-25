55691e8c7329ecd389d7bf1e92ddfc6388999549
Brandon Vincent <brandonvincent567@gmail.com>
2021-07-25 21:37:24 +1000

    added all lifecycle methods

--- demo1/src/App.js
+++ demo1/src/App.js
@@ -1,11 +1,40 @@
 import BtnFunction from "./BtnFunction";
 import BtnClass from "./BtnClass";
+import React, { useState } from "react";
 
 function App() {
+  const [showClass, setClass] = useState(false);
+  const [count, addCounter] = useState(0);
+  const [toggleClass, setToggle] = useState(true);
   return (
     <div className="App">
+      <div>
+        <h1> Counter : {count}</h1>
+        <button onClick={() => addCounter(count + 1)}> + </button>
+        <button onClick={() => addCounter(count - 1)}> - </button>
+      </div>
       <BtnFunction></BtnFunction>
-      <BtnClass></BtnClass>
+      <button
+        onClick={() => {
+          setClass(true);
+        }}
+      >
+        Lock render for children
+      </button>
+      <button
+        onClick={() => {
+          setClass(false);
+        }}
+      >
+        Unlock render for children
+      </button>
+      {toggleClass ? (
+        <button onClick={() => setToggle(false)}> Delete Class </button>
+      ) : (
+        <button onClick={() => setToggle(true)}> Add class </button>
+      )}
+      {toggleClass && <BtnClass count={count} locked={showClass}></BtnClass>}
+
       <h1> Here </h1>
     </div>
   );
--- demo1/src/BtnClass.js
+++ demo1/src/BtnClass.js
@@ -8,6 +8,8 @@ class BtnClass extends Component {
     this.state = {
       name: null,
       loggedIn: false,
+      users: ["Josh", "Samantha", "Bob", "Alice"],
+      message: null,
     };
   }
 
@@ -15,7 +17,7 @@ class BtnClass extends Component {
   //     console.log("Get Derived State From Props");
   //     console.log(props);
   //     console.log(state);
-  //     return { name: props.nameFromParent };
+  //     return { this.setState({name: props.name })};
   //   }
 
   login = () => {
@@ -26,30 +28,64 @@ class BtnClass extends Component {
     this.setState({ loggedIn: false });
   };
 
+  deleteUser = (name) => {
+    console.log("called");
+    this.setState({ users: this.state.users.filter((user) => name !== user) });
+  };
+
   componentDidMount() {
+    // api call
     console.log("Component did mount");
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    // update only if count > 0;
+    // return nextProps.count >= 0;
+    return !nextProps.locked;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    // if previous state is == sth then change accordingly
+    return null;
+  }
+
+  componentDidUpdate() {
+    alert("Component did update");
+  }
+
+  componentWillUnmount() {
+    alert("Unmounted");
+  }
   render() {
     return (
       <div className="btn__class__wrapper">
         <h1>Class</h1>
-
+        Count: {this.props.count}
         {this.state.loggedIn ? (
-          <div>
+          <div className="logoutContainer">
             <h1>You are logged in </h1>
             <button className="login" onClick={this.logout}>
               Press here to log out
             </button>
           </div>
         ) : (
-          <div>
+          <div className="loginContainer">
             <h1>You are logged out </h1>
             <button className="login" onClick={this.login}>
               Press here to log in
             </button>
           </div>
         )}
+        <div className="users">
+          {this.state.users.map((user) => (
+            <div>
+              <h1> {user}</h1>
+              <button onClick={() => this.deleteUser(user)}>
+                Delete this User{" "}
+              </button>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
